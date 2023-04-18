e1253640f2d5339f85f9c3ec82f5921292f766c4
malik672 <aremumalik01@gmail.com>
2023-04-18 19:21:58 +0100

    huff

---
+++ src/code.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+abstract contract Counter {
+    enum mySpace {
+        VALUE1,
+        VALUE2,
+        VALUE3
+    }
+}
--- src/codes.sol
+++ src/codes.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-  enum mySpace {VALUE1, VALUE2, VALUE3}
+    enum mySpace {
+        VALUE1,
+        VALUE2,
+        VALUE3
+    }
 }
