5dfaf80955f84f3246c005ed6d9b2641e8e1c186
malik672 <aremumalik01@gmail.com>
2023-04-22 11:50:50 +0100

    executor

--- src/codes.sol
+++ src/codes.sol
@@ -2,9 +2,20 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    enum mySpace {
-        VALUE1,
-        VALUE2,
-        VALUE3
+    function execute(address to, uint256 value, bytes memory data, uint8 red, uint256 txGas)
+        internal
+        returns (bool success)
+    {
+        if (value == red) {
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                success := delegatecall(txGas, to, add(data, 0x20), mload(data), 0, 0)
+            }
+        } else {
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                success := call(txGas, to, value, add(data, 0x20), mload(data), 0, 0)
+            }
+        }
     }
 }
